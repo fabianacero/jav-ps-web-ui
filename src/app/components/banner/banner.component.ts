610ef253933cecca99c98bc75a623143806126da
Fabian Acero Garcia <facero@bancodebogota.com.co>
2020-04-18 15:58:01 -0500

    Ajuste de estilos y merge

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -21,12 +21,12 @@ export class BannerComponent implements OnInit {
     let pointer = 0;
     const figures = this.getBannerElements();
     for (let i = 0; i < figures.length; i++) {
-      if (figures[i].className == 'visible') {
+      if (figures[i].className === 'visible') {
         figures[i].className = 'hidden';
         pointer = i;
       }
     }
-    if (++pointer == figures.length) {
+    if (++pointer === figures.length) {
       pointer = 0;
     }
     figures[pointer].className = 'visible';
